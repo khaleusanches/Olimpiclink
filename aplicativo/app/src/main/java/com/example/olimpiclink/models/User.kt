8f25586974195a3f37c6399e8fc85b62ea3ca100
KhaleuMancini <131072913+KhaleuMancini@users.noreply.github.com>
2024-07-25 21:18:33 -0300

    Delete aplicativo directory

--- aplicativo/app/src/main/java/com/example/olimpiclink/models/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.olimpiclink.models
-
-class User(username: String, password: String, eemail: String) {
-    public var nome = username;
-    var senha = password;
-    var email = eemail;
-}
