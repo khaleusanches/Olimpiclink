51038c93ea6852f48d3df81acbb54ce4ec41903d
khaleusanches <khaleusanches@gmail.com>
2024-09-07 02:46:27 -0300

    Ajeitando layout das telas já criadas, terminado o sistema de login e da tela 1 de registro

---
+++ Programa/Codigo/DadosAPI/olimpiclink.database/olimpiclink.database/Models/BadRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace olimpiclink.database.Models
+{
+    public class BadRequest
+    {
+        string texto {  get; set; }
+        public BadRequest(string texto) { this.texto = texto; }
+    }
+}
--- Programa/Codigo/app/src/main/java/devsystem/olimpiclink/util/EndpointUser.kt
+++ Programa/Codigo/app/src/main/java/devsystem/olimpiclink/util/EndpointUser.kt
@@ -1,15 +1,22 @@
 package devsystem.olimpiclink.model.util
 
+import devsystem.olimpiclink.model.RequestMessages
 import devsystem.olimpiclink.model.User
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
-interface Endpoint {
+interface EndpointUser {
     @GET("api/users/login/{login_user}&{password_user}")
     fun userLogin(
         @Path("login_user") login_user : String,
         @Path("password_user") password_user : String
     ) : Call<User>
+
+    @GET("api/users/check/{login_user}&{email_user}")
+    fun emailUsernameCheck(
+        @Path("login_user") login_user: String,
+        @Path("email_user") email_user: String,
+    ) : Call<RequestMessages>
+
 }
